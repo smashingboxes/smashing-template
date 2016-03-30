48ff1a467a287ea82e34fd4647b8b5e08847eab1
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-03-30 13:36:58 -0400

    Remove global variable for path

--- lib/database_generator.rb
+++ lib/database_generator.rb
@@ -2,16 +2,16 @@ require_relative './file_creator.rb'
 
 def database_set_up
   remove_file "config/database.yml"
-  file 'config/database.yml', render_file("#{$path}/files/database.yml")
+  file 'config/database.yml', render_file(File.join(File.dirname(__FILE__), "files/", "database.yml"))
   gsub_file 'config/database.yml', /app_name/, app_name
 end
 
 def travis_set_up
   run "cp config/database.yml config/database.example.yml"
   run "cp config/secrets.yml config/secrets.example.yml"
-  file '.travis.yml', render_file("#{$path}/files/.travis.yml")
+  file '.travis.yml', render_file(File.join(File.dirname(__FILE__), "files/", ".travis.yml"))
   gsub_file '.travis.yml', /app_name/, app_name
-  file '.rubocop.yml', render_file("#{$path}/files/.rubocop.yml")
+  file '.rubocop.yml', render_file(File.join(File.dirname(__FILE__), "files/", ".rubocop.yml"))
   if @cucumber_capybara
     inject_into_file '.travis.yml', after: "- bundle exec rspec\n" do
       <<-RUBY
--- lib/file_creator.rb
+++ lib/file_creator.rb
@@ -1,5 +1,3 @@
-$path = File.expand_path(File.dirname(__FILE__))
-
 def render_file(path)
   IO.read(path)
 end
