16b5bebfdc1274eb86a02fb85ec11fc6ac943fb8
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-03-30 16:13:51 -0400

    Attempt at passing travis for template
    Update Travis file.
    
    Configure travis yml file to check generated app
    
    Add install rails gem to travis.
    
    Added echo commands.
    
    Testing where we are at in the template.
    
    Fix broken path to template.
    
    Remove echo and try just n command.
    
    Trying delcarations instead.
    
    BAHHH
    
    Add interactive yes/no commands to travis file.
    
    Remove database command since database created in template.
    
    Add mkdir features command to copy in files.
    
    Rename example app.
    
    Add test db:setup to fix rake cucumber error.
    
    Try to send yes's to all questions to pass cucumber spec
    
    Try with printf
    
    Remove mkdir features line.

--- lib/files/database_cleaner.rb
+++ lib/files/database_cleaner.rb
@@ -1,21 +1,12 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:deletion)
-  end
-
-  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :deletion
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
