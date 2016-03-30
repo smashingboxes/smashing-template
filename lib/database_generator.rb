43949984f708ab880e6e58f4898afa55a94af15c
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-03-30 13:46:53 -0400

    Fix file path repetitiveness with method.

--- lib/database_generator.rb
+++ lib/database_generator.rb
@@ -2,16 +2,16 @@ require_relative './file_creator.rb'
 
 def database_set_up
   remove_file "config/database.yml"
-  file 'config/database.yml', render_file(File.join(File.dirname(__FILE__), "files/", "database.yml"))
+  file 'config/database.yml', render_file(path("database.yml"))
   gsub_file 'config/database.yml', /app_name/, app_name
 end
 
 def travis_set_up
   run "cp config/database.yml config/database.example.yml"
   run "cp config/secrets.yml config/secrets.example.yml"
-  file '.travis.yml', render_file(File.join(File.dirname(__FILE__), "files/", ".travis.yml"))
+  file '.travis.yml', render_file(path(".travis.yml"))
   gsub_file '.travis.yml', /app_name/, app_name
-  file '.rubocop.yml', render_file(File.join(File.dirname(__FILE__), "files/", ".rubocop.yml"))
+  file '.rubocop.yml', render_file(path(".rubocop.yml"))
   if @cucumber_capybara
     inject_into_file '.travis.yml', after: "- bundle exec rspec\n" do
       <<-RUBY
