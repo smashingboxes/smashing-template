c4a11b61280162ecf83c1696545832be979fe2ab
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-10-20 09:31:17 -0400

    Modify template to check for Rails version.

--- template.rb
+++ template.rb
@@ -8,38 +8,41 @@ require_relative 'lib/git_initializer.rb'
 # -----------------------------
 # CREATE TEMPLATE
 # -----------------------------
-remove_gemfile
-# -----------------------------
-# API ONLY APP?
-# -----------------------------
-if yes?("Is this an API app? (y/n)")
-  api_gemfile
-  if yes?("Does this API app have an admin interface? (y/n)")
-    api_with_admin_install
+rails_version="$(rails -v | cut -d ' ' -f 2)"
+if rails_version == '5.0.0.1'
+  remove_gemfile
+  # -----------------------------
+  # API ONLY APP?
+  # -----------------------------
+  if yes?("Is this an API app? (y/n)")
+    api_gemfile
+    if yes?("Does this API app have an admin interface? (y/n)")
+      api_with_admin_install
+    else
+      api_only_install
+    end
   else
-    api_only_install
+    integrated_app_install
   end
-else
-  integrated_app_install
+  # -----------------------------
+  # DATABASE
+  # -----------------------------
+  database_set_up
+  travis_set_up
+  git_ignore_append
+  # -----------------------------
+  # GEM ADDITIONS (OPTIONAL)
+  # -----------------------------
+  add_gem_configs
+  install_optional_gems
+  # -----------------------------
+  # SETUP
+  # -----------------------------
+  rubocop_clean_up
+  remove_test_dir
+  create_database
+  initialize_git
 end
-# -----------------------------
-# DATABASE
-# -----------------------------
-database_set_up
-travis_set_up
-git_ignore_append
-# -----------------------------
-# GEM ADDITIONS (OPTIONAL)
-# -----------------------------
-add_gem_configs
-install_optional_gems
-# -----------------------------
-# SETUP
-# -----------------------------
-rubocop_clean_up
-remove_test_dir
-create_database
-initialize_git
 # -----------------------------
 # COMPLETE
 # -----------------------------
