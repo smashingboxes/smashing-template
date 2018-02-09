cb82c89b0e29ee65261bfe9fa50fe9daa23b40c4
Derek Kniffin <derek.kniffin@gmail.com>
2018-02-09 14:19:24 -0500

    Make frozen_string_literal a global config

--- lib/boxcar/version.rb
+++ lib/boxcar/version.rb
@@ -1,8 +1,7 @@
 module Boxcar
-  RAILS_VERSION = "~> 5.1.4".freeze
+  RAILS_VERSION = "~> 5.1.4"
   RUBY_VERSION = IO
                  .read("#{File.dirname(__FILE__)}/../../.ruby-version")
                  .strip
-                 .freeze
-  VERSION = "0.0.1".freeze
+  VERSION = "0.0.1"
 end
