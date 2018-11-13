771b92782a511e11b77e4f134e359a1566355a02
Edward Kerr <edd.kerr@smashingboxes.com>
2018-11-13 15:47:07 -0500

    remove typo

--- lib/boxcar/app_builder.rb
+++ lib/boxcar/app_builder.rb
@@ -3,7 +3,7 @@
 # The purpose of this class is to provide the building blocks that are used for generating a rails
 # app. For example, the readme method is responsible for creating the readme file. Since we're
 # inheriting from Rails::AppBuilder, we have a lot of those building blocks set up for us already.
-# We can override those methods to customize how they work, or we can create new ones.=
+# We can override those methods to customize how they work, or we can create new ones.
 module Boxcar
   class AppBuilder < Rails::AppBuilder
     def gitignore
