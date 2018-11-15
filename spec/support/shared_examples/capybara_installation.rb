fac1fcdbb4aa81c3aa866d2777f53976202a322b
Derek Kniffin <derek.kniffin@gmail.com>
2018-11-15 10:47:53 -0500

    Tweak a test

--- spec/support/shared_examples/capybara_installation.rb
+++ spec/support/shared_examples/capybara_installation.rb
@@ -3,13 +3,14 @@
 shared_examples_for "a run that installs capybara" do
   it "installs capybara" do
     expect(gemfile).to match(/^\s+gem "capybara"/)
-
-    expect(File).to exist("#{project_path}/app/views/shared/home.html.erb")
-    expect(File).to exist("#{project_path}/spec/system/home_page_spec.rb")
     expect(File).to exist("#{project_path}/spec/support/capybara.rb")
   end
 
   it "installs selenium-webdriver" do
     expect(gemfile).to match(/^\s+gem "selenium-webdriver"/)
+  end
+
+  it "creates a boilerplate home page spec" do
+    expect(File).to exist("#{project_path}/spec/system/home_page_spec.rb")
   end
 end
