665368215a550603267a098d1c7c78756a301e55
Derek Kniffin <derek.kniffin@gmail.com>
2018-11-16 13:51:07 -0500

    Fix react component rendering

--- templates/boxcar/app/javascript/packs/hello_world.jsx
+++ templates/boxcar/app/javascript/packs/hello_world.jsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 
 import HelloWorld from 'hello_world';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('hello_world_rootroot'));
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('hello_world_root')
+  );
+});
