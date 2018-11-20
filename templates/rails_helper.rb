80cc0014bc2894cf446869b9cc0a836adb112082
Edward Kerr <edd.kerr@smashingboxes.com>
2018-11-20 11:07:51 -0500

    Update travis and rubocop configs

--- templates/rails_helper.rb
+++ templates/rails_helper.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "selenium/webdriver"
 require "spec_helper"
 require "support/request_helpers"
 
@@ -19,6 +22,16 @@ Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.register_driver :chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[--no-sandbox --headless --disable-gpu --disable-dev-shm-usage]
+  )
+  Selenium::WebDriver::Chrome.driver_path = `which chromedriver`
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :headless_chrome # or optional :chrome for getting a browser locally
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
