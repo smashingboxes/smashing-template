b12335efcdf9e2cb0a7bb1ae43aed75bd8c7c1c1
Edward Kerr <edd.kerr@smashingboxes.com>
2018-11-29 09:25:26 -0500

    move capybara configs into capybara.rb

--- templates/rails_helper.rb
+++ templates/rails_helper.rb
@@ -23,16 +23,6 @@ Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(
-    args: %w[--no-sandbox --headless --disable-gpu --disable-dev-shm-usage]
-  )
-  Selenium::WebDriver::Chrome.driver_path = `which chromedriver`
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.javascript_driver = :headless_chrome # or optional :chrome for getting a browser locally
-
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
