d3fd64680c93c6c489f060193e0d345bd14f5964
Tyler Rockwell <tyler.rockwell87@gmail.com>
2018-12-05 13:30:46 -0500

    Properly set Capybara driver in system specs

--- templates/boxcar/spec/support/capybara.rb
+++ templates/boxcar/spec/support/capybara.rb
@@ -1,9 +1,11 @@
-Capybara.register_driver :chrome do |app|
+Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(
     args: %w[--no-sandbox --headless --disable-gpu --disable-dev-shm-usage]
   )
-  Selenium::WebDriver::Chrome.driver_path = `which chromedriver`
+  Selenium::WebDriver::Chrome.driver_path = `which chromedriver`.chomp
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.javascript_driver = :headless_chrome # or optional :chrome for getting a browser locally
+# Change this to :chrome to open a local browser
+# driven_by option must also be changed to :chrome in spec_helper.rb
+Capybara.javascript_driver = :headless_chrome
