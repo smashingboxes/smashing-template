cd116a70ec719c6def41a8341ce9028a1260efac
Tyler Rockwell <tyler.rockwell87@gmail.com>
2018-12-06 14:06:18 -0500

    Improve initial site aesthetic

--- templates/boxcar/app/controllers/application_controller.rb
+++ templates/boxcar/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def home
     flash[:notice] = "You have flash messages in your header!"
-    flash[:alert] = "You you might want to remove this in application_controller.rb #home!"
+    flash[:alert] = "You might want to remove this in application_controller.rb #home!"
     render "shared/home"
   end
 end
